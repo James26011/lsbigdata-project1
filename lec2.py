a8503bc79653115aa3a42ce9f13ef752fdf5d948
James26011 <huen3323@naver.com>
2024-07-10 17:41:34 +0900

    project init

---
+++ lec2.py
@@ -0,0 +1,89 @@
+a = 10
+b = 3.3
+# 기본 산술 연산자
+print("a + b =", a + b) # 덧셈
+print("a - b =", a - b) # 뺄셈
+print("a * b =", a * b) # 곱셈
+print("a / b =", a / b) # 나눗셈
+print("a % b =", a % b) # 나머지
+print("a // b =", a // b) # 몫
+print("a ** b =", a ** b) # 거듭제곱
+print("a ** b =", a ** b) # 거듭제곱
+print("a ** b =", a ** b) # 거듭제곱
+
+x = (2**4) + (12453 // 7)
+x % 8 < (9**7/12)*(36452%253)
+
+# 사용자 나이 검증 예제
+user_age = 14
+is_adult = user_age >= 18
+print("성인입니까?", is_adult)
+
+a = "True" #문자열
+b = TRUE #변수로 인식되었지만, 정의되지 않았다.
+c = true #변수로 인식되었지만, 정의되지 않았다.
+d = True #예약어
+
+# True, Fales
+a = True
+b = False
+a and b
+
+# and
+True and True
+False and False
+
+# or
+True or True
+True or False
+False or True
+False or False
+
+# 1,0
+True + True
+True + False
+False + False
+
+True * True
+True * False
+False * False
+
+a = False
+b = False
+min(a+b,1)
+
+#복합 대입 연산자
+a = 3
+a += 10 # a = a + 10
+a
+
+a -= 4
+a %= 3
+a += 12
+a **= 2
+a /= 7
+
+str1 = "hello"
+str2 = "world"
+str1 + str1
+str1 * 3 
+
+# 단항 연산자
+x = 13
++x
+-x
+~x
+bin(x)
+bin(~x)
+
+#binary 이진법
+bin(5)
+bin(4)
+
+pip install pydataset
+
+import pydataset
+pydataset.data()
+df = pydataset.data("AirPassengers")
+df
+
